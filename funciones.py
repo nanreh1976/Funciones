5d1a72c0721491bfcd6781a4ebca11e9b3c7bff7
Hernan Gomez <95485080+nanreh1976@users.noreply.github.com>
2022-10-19 21:41:26 -0300

    funciones

---
+++ funciones.py
@@ -0,0 +1,51 @@
+# Funciones en python
+
+def suma(num1, num2): #parametros
+    return num1 + num2
+
+def division(num1,num2):
+    return num1 / num2
+
+print("la suma es : ", suma(2,3)) #(2,3) son argumentos
+print("la suma es : ", suma(7,6))
+
+print("la suma es : ", 8+8)
+
+print("la division 3/7 es : ", division(3,7))
+
+print("la division 7/3 es : ", division(num2=3,num1=7))
+
+# funciones con parametros  por default
+#calculadora de alimento mensual y dia por animal
+
+cantConsDia = float(input("cuanto consume por dia en gramos : "))
+precioEst = float(input("cuanto cuesta el kg de alimento : "))
+print("#"*80)
+print("Para terminar complete con 0 ambos ingresos de datos")
+print("#"*80)
+
+
+def costoKg(precioEst, kilosMensuales) :
+        costoTotal = precioEst * kilosMensuales
+        return costoTotal
+
+def kilos(gramos, dias=30) :
+     gramosMensual =gramos * dias
+     kilosMensual = gramosMensual/1000
+     return kilosMensual 
+
+while (cantConsDia !=0 and precioEst !=0):
+    print("En el mes, el animal va a consumir ", kilos(cantConsDia)," kilos ")
+    print("En el mes, vas a gastar en el animal  ", costoKg(precioEst , kilos(cantConsDia)), "pesos")
+    cantConsDia = float(input("cuanto consume por dia en gramos : "))
+    precioEst = float(input("cuanto cuesta el kg de alimento : "))
+    print("#"*60)
+    print("Para terminar complete con 0 ambos ingresos de datos")
+    print("#"*60)
+print("#"*30)
+print ("Fin programa Funciones alimento mascota")
+print("#"*30)
+
+
+
+
