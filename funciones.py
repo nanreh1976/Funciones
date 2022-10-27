9422d09dd08865302d03231e3c5ea61a212b872a
Hernan Gomez <95485080+nanreh1976@users.noreply.github.com>
2022-10-27 12:02:05 -0300

    ultima clase de funciones

--- funciones.py
+++ funciones.py
@@ -64,6 +64,41 @@ print("El resultado de la suma es: ", resultadoSuma)
 #var1 = 67
 #print("lo que hay en VAR1 es: ",var1)
 
-print("la suma entre 3 y 3 es : ", sumaEnteros(3,3))
+print("la suma entre 3 y 3 es : ", sumaEnteros(3,3)) #6
 
 
+#argumrnyod por valor y por referencia
+
+#por valor(trabajamos con respresentaciones de esos valores cuando los valores son )
+#int, str, boo,float
+
+#cuando se trabaja con un argumento por valor no se modifica el dato como tal al ser porcesado por la lista
+
+def triplicarNumero(numero):
+    return numero *3
+
+def triplicarNumero2(numero):
+    numero= numero *3
+    return numero
+
+#num1 = 2
+
+#print(triplicarNumero(num1))
+#print(num1)
+
+numero=2
+print(triplicarNumero2(numero))
+print (numero)
+
+#Argumentos por referencia
+
+
+def duplicarArray(array):
+    for i in range(len(array)):
+        array[i]=array[i] *2
+    return array
+
+miArray = [2,4,6]
+print("mi array antes de la funcion : ", miArray)
+print("mi array con la funcion :", duplicarArray(miArray))
+print("como quedo mi array despues : ",miArray)    
